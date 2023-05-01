c0a9ad15c13d7fe701b0ec75b04f468fe76ad125
rozlette <Rozelette@users.noreply.github.com>
2023-05-01 16:56:17 -0500

    qwe

--- extern/ZAPDUtils/Vec3s.h
+++ extern/ZAPDUtils/Vec3s.h
@@ -2,20 +2,19 @@
 
 #include <cstdint>
 
-struct Vec3s
-{
-	int16_t x, y, z;
+namespace ZAPDUtils {
+struct Vec3s {
+    int16_t x, y, z;
 
-	Vec3s()
-	{
-		x = 0;
-		y = 0;
-		z = 0;
-	};
-	Vec3s(int16_t nX, int16_t nY, int16_t nZ)
-	{
-		x = nX;
-		y = nY;
-		z = nZ;
-	};
+    Vec3s() {
+        x = 0;
+        y = 0;
+        z = 0;
+    };
+    Vec3s(int16_t nX, int16_t nY, int16_t nZ) {
+        x = nX;
+        y = nY;
+        z = nZ;
+    };
 };
+} // namespace ZAPDUtils
